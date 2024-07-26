04265f1c80c85aeedb26f117b6f1b4f9461ce82b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-26 23:33:45 +0530

    use productId in product page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
-  
+  const {productId} = useParams();
   return (
     <div>
 
