76f0ea96fd2279576c1fd2019c3c3efa11bdec5b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-27 01:22:29 +0530

    set new and old price

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -28,6 +28,14 @@ const ProductDisplay = (props) => {
                     <img src={star_dull_icon} alt='' />
                     <p>(122)</p>
                 </div>
+                <div className="productdisplay-right-prices">
+                    <div className="productdisplay-right-price-old">
+                        ${product.old_price}
+                    </div>
+                    <div className="productdisplay-right-price-new">
+                        ${product.new_price}
+                    </div>
+                </div>
         </div>
     </div>
   )
