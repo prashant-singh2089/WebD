b9b248e8a5f6266c37af2aeb0f37dd49053b04de
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-27 00:45:30 +0530

    change some

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
-import Breadcrum from '../Components/Breadcrum/Breadcrum'
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
 
 const Product = () => {
-  const {all_product} = useContext(ShopContext)
+  const {all_product} = useContext(ShopContext);
   const {productId} = useParams();
   const product = all_product.find((e)=> e.id === Number(productId));
   return (
