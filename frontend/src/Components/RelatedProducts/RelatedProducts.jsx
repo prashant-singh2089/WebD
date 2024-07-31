da69e4c87df2e9d42ddcb51fab4f1605d1147bda
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-31 20:07:48 +0530

    add data_product in Assets folder

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './RelatedProducts.css'
+import data_product from '../Assets/data'
 
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
-        
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i)=>{
+
+            })}
+        </div>
     </div>
   )
 }
