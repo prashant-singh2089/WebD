4e97d0fa808144aa29471b4710994a1f4251b74b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-31 19:06:32 +0530

    create descriptionbox-navigator

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -4,7 +4,11 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box-fade">Reviews (122)</div>
+        </div>
+        
     </div>
   )
 }
