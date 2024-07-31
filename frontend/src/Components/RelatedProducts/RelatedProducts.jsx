cd660dfc5fd1dab23a7d6a8e3e9e7313a4a7aa8a
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-31 20:13:47 +0530

    add props

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './RelatedProducts.css'
 import data_product from '../Assets/data'
+import Item from '../Items/Item'
 
 const RelatedProducts = () => {
   return (
@@ -9,7 +10,7 @@ const RelatedProducts = () => {
         <hr />
         <div className="relatedproducts-item">
             {data_product.map((item,i)=>{
-
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
             })}
         </div>
     </div>
