67934b8158101fee9d1badfeddbbd3b0755b9525
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-31 20:56:01 +0530

    create addToCart function

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = ()=>{
     let cart = {};
-    for(let index = 0; index < all_product.length; index++){
+    for(let index = 0; index < all_product.length+1; index++){
         cart[index] = 0;
     }
     return cart;
@@ -16,6 +16,12 @@ const ShopContextProvider = (props) => {
     const [cartItems,setCartItems] = useState(getDefaultCart());
     const contextValue = {all_product,cartItems};
     
+    const addToCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+    }
+
+    
+
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
