a4235e5c90af7e6392023be2d8bbd341ff130c99
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-31 22:03:51 +0530

    check when Add to cart button click function

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -18,6 +18,7 @@ const ShopContextProvider = (props) => {
     
     const addToCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
     }
 
     const removeFromCart = (itemId) =>{
