c617ac1e2605b0bcbf9ba449a1df5885911e4292
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-05 21:05:19 +0530

    Database connection with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,6 @@ const path = require("path");
 const cors = require("cors");
 app.use(express.json()); //request to response that will automatically pass through the json
 app.use(cors()); //react project connect to express app to 4000 port
+
+// Database connection with MongoDB
+mongoose.connect("mongodb+srv://ecommerceweb:ecom1234@cluster0.553bf.mongodb.net/e-commerce");
