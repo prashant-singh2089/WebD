02fb6ca72f74f0ab4bcbb0ae562f00d3ae63c2d1
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-05 22:38:49 +0530

    add the product to database

--- backend/index.js
+++ backend/index.js
@@ -79,6 +79,25 @@ const Product = mongoose.model("Product",{
     },
 })
 
+//add the product to database
+
+app.post('/addproduct',async (req,res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
 
 app.listen(port,(error)=>{
     if(!error){
