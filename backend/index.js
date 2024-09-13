49ba3442b7d7a2d9e7bf937397394cba4ad47a08
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-13 20:28:58 +0530

    Creating Upload Endpoint for images

--- backend/index.js
+++ backend/index.js
@@ -74,6 +74,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
+//Creating Upload Endpoint for images
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
 
 
 
