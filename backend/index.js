11bf6972a7acebb54aca43927b4dd65e3a2c2b25
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-13 16:16:37 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -6,142 +6,24 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const { error } = require("console");
+const exp = require("constants");
 
-app.use(express.json()); //request to response that will automatically pass through the json
-app.use(cors()); //react project connect to express app to 4000 port
+app.use(express.json());
+app.use(cors());
 
-// Database connection with MongoDB
-//mongoose.connect("mongodb+srv://ecommerceweb:ecom1234@cluster0.553bf.mongodb.net/e-commerce");
-//mongodb+srv://ecommerceweb:@cluster0.553bf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// Database connection
-mongoose.connect("mongodb+srv://ecommerceweb:ecom1234@cluster0.553bf.mongodb.net/full-stack-e-commerce-website", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch(err => {
-    console.error('MongoDB connection error:', err);
-  });
+//Databse connction with mongodb
+mongoose.connect("mongodb+srv://ecomweb:ecom1234@cluster0.6ah2w.mongodb.net/e-commerce");
 
 //API creation
 
 app.get("/",(req,res)=>{
-    res.send("Express App is Running")
-})
-
-//Image Storage Engine
-
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename: (req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
-const upload = multer({storage:storage})
-
-//Creating Upload Endpoint for images
-
-app.use('/images',express.static('upload/images'))
-
-app.post("/upload",upload.single('product'),(req,res)=>{
-    res.json({
-        success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-})
-
-// Schema for creating products
-
-const Product = mongoose.model("Product",{
-    id:{
-        type:Number,
-        required:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    image:{
-        type:String,
-        required:true,
-    },
-    category:{
-        type:String,
-        required:true,
-    },
-    new_price:{
-        type:Number,
-        required:true,
-    },
-    old_price:{
-        type:Number,
-        required:true,
-    },
-    date:{
-        type:Date,
-        default:Date.now,
-    },
-    avilable:{
-        type:Boolean,
-        default:true,
-    },
-})
-
-//add the product to database
-
-app.post('/addproduct',async (req,res)=>{
-    // generate id automatically
-    let products = await Product.find({});
-    let id;
-    if(products.length>0)
-    {
-        let last_product_array = products.slice(-1);
-        let last_product = last_product_array[0];
-        id = last_product.id+1;
-    }
-    else{
-        id=1;
-    }
-    const product = new Product({
-        id:id,
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price,
-    });
-    console.log(product);
-    await product.save();
-    console.log("Saved");
-    res.json({
-        success:true,
-        name:req.body.name,
-    })
-})
-
-//Creating API for deleting Products
-app.post('/removeproduct',async (req,res)=>{
-    await Product.findOneAndDelete({id:req.body.id});
-    console.log("Removed");
-    res.json({
-        success:true,
-        name:req.body.name
-    })
-})
-
-//Creating API for getting all Products
-app.get('/allproducts',async (req,res)=>{
-    let products = await Product.find({});
-    console.log("All products Fetched");
-    res.send(products);
+    res.send("Express App is running")
 })
 
 
 app.listen(port,(error)=>{
     if(!error){
-        console.log("server Running on Port "+port)
+        console.log("Server Running on port "+port)
     }
     else{
         console.log("Error : "+error)
