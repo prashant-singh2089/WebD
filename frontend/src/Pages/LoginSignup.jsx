36858cc92ca01badff032415fa2f3a500825f265
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-14 18:02:40 +0530

    create API for login Signup

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -4,17 +4,27 @@ import './CSS/LoginSignup.css'
 const LoginSignup = () => {
 
   const [state,setState] = useState("Login");
+  const [formData,setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
 
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state==="Sign Up"?<input type='text' placeholder='Your Name' />:<></>}
-          <input type='email' placeholder='Email Address' />
-          <input type='password' placeholder='Password' />
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type='text' placeholder='Your Name' />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type='email' placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type='password' placeholder='Password' />
         </div>
-        <button>Continue</button>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
         {state==="Sign Up"?
         <p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>:
         <p className='loginsignup-login'>Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
