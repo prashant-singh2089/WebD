118c4dbb88638fa018b6194f304233f2f80af65f
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-14 20:06:30 +0530

    remove new_collections after that not display products

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './NewCollections.css'
-import new_collections from '../Assets/new_collections'
 import Item from '../Items/Item'
 
 const NewCollections = () => {
