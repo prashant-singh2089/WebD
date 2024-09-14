02cffbde9082570e3989cbff7ff494e9c317fc88
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-14 19:08:55 +0530

    getting the data an input field

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,6 +17,24 @@ const LoginSignup = () => {
   // add the API for login
   const login = async () =>{
     console.log("login Function Executed",formData);
+    // getting the data an input field
+    let responseData;
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=> response.json()).then((data)=>responseData=data)
+
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors);
+    }
   } 
 
   // add the API for signup
