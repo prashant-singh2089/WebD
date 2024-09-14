13cc45cf228e5211baf276881706a32ac9d6f727
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-14 12:41:30 +0530

    create function to remove product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,18 @@ const ListProduct = () => {
       fetchInfo();
     },[])
 
+    const remove_product = async (id)=>{
+      await fetch('http://localhost:4000/removeproduct',{
+        method: 'POST',
+        headers:{
+          Accept:'application/json',
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify({id:id})
+      })
+      await fetchInfo();
+    }
+
   return (
     <div className='list-product'>
       <h1>All Products List</h1>
