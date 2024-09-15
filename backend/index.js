eaa426ded9ac98b4754dc9c803effda0326c8d9c
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-15 11:29:11 +0530

    creating middleware to fetch user

--- backend/index.js
+++ backend/index.js
@@ -242,6 +242,24 @@ app.get('/popularinwomen', async (req,res)=>{
     res.send(popular_in_women);
 })
 
+// creating middleware to fetch user
+    const fetchUser = async (req,res,next)=>{
+        const token = req.header('auth-token');
+        if(!token) {
+            res.status(401).send({errors:"Please authenticate using valid token"});
+        }
+        else{
+            try{
+                const data = jwt.verify(token,'secret_ecom');
+                req.user = data.user;
+                next();
+            }
+            catch (error){
+                res.status(401).send({errors:"please authenticate using a valid token"});
+            }
+        }
+    }
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on port "+port)
