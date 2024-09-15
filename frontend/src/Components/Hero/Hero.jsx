f54ade3e62a950a6e199a3b6a3c08b4377e99642
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-16 00:13:14 +0530

    check

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/h.webp'
+import hero_image from '../Assets/hero.webp'
 const Hero = () => {
   return (
     <div className='hero'>
