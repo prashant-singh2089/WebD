0e6d69d6914b85747a48e1ca3b990fca20243a13
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-09-15 11:28:58 +0530

    creating endpoint for popular in women section

--- backend/index.js
+++ backend/index.js
@@ -234,6 +234,14 @@ app.get('/newcollections',async (req,res)=>{
     res.send(newcollection);
 })
 
+//creating endpoint for popular in women section
+app.get('/popularinwomen', async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women);
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on port "+port)
